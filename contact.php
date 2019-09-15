1532989a02334fab8221058d3f677116e1a71055
Dhanushga Lionel <32670499+DhanushgaLionel@users.noreply.github.com>
2019-09-15 00:05:32 -0400

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,15 +1,15 @@
 <?php
 $errors = '';
-$myemail = 'yourname@website.com';//<-----Put Your email address here.
+$myemail = 'danny.lionel1@gmail.com';//<-----Put Your email address here.
 if(empty($_POST['name'])  ||
    empty($_POST['email']) ||
-   empty($_POST['message']))
+   empty($_POST['subject']))
 {
     $errors .= "\n Error: all fields are required";
 }
 $name = $_POST['name'];
 $email_address = $_POST['email'];
-$message = $_POST['message'];
+$message = $_POST['subject'];
 if (!preg_match(
 "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
 $email_address))
@@ -28,6 +28,6 @@ $headers = "From: $myemail\n";
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
 //redirect to the 'thank you' page
-header('Location: contact-form-thank-you.html');
+header('Location: contact.html');
 }
 ?>
