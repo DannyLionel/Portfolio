ee5a94ad85404985db750cf39008b365d4bfa272
Dhanushga Lionel <32670499+DhanushgaLionel@users.noreply.github.com>
2019-09-16 13:34:22 -0400

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,6 +1,6 @@
 <?php
 $errors = '';
-$myemail = 'danny.lionel1@gmail.com';//<-----Put Your email address here.
+$myemail = 'dhanushga.lionel@outlook.com';//<-----Put Your email address here.
 if(empty($_POST['name'])  ||
    empty($_POST['email']) ||
    empty($_POST['subject']))
